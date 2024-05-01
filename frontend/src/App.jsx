68ffaa8b67f7b908e83396df1f391ce59fde0950
Oulaya omar <oulaya.omar@gmail.com>
2024-05-02 01:53:34 +0200

    2.1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react'
+import './App.css';
+import ContactList from './ContactList';
+
+function App() {
+  const [contacts, setContacts]= useState([
+    // {"firstName" : "Tim", "lastName":"John", "email":"email", id :1}
+  ]);
+
+  useEffect(()=>{
+    // fetchContacts();
+  },[])
+
+  const fetchContacts = async ()=>{
+    const response = await fetch("http://127.0.0.1:5000/contacts/")
+    const data = await response.json();
+    setContacts(data.contacts);
+    // console.log(data.contacts);
+  }
+
+  return <ContactList contacts={contacts}/>
+  
+}
+
+export default App
---
+++ frontend/src/ContactForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useState } from 'react';
+
+function ContactForm({}) {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+  return <form>
+    <div>
+       <></> 
+    </div>
+  </form>
+}
+
+export default ContactForm
